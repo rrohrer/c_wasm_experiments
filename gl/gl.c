4b0af4d4a5e28f1297f15d995833eab112f8e305
rrohrer <ryan.rohrer@gmail.com>
2020-04-19 22:30:28 -0700

    added a simple opengl example

---
+++ gl/gl.c
@@ -0,0 +1,17 @@
+#define GL_DEPTH_BUFFER_BIT 0x00000100
+#define GL_STENCIL_BUFFER_BIT 0x00000400
+#define GL_COLOR_BUFFER_BIT 0x00004000
+void glViewport(int x, int y, int width, int height);
+void glClearColor(float r, float g, float b, float a);
+void glClear(unsigned int);
+
+void init_engine() { glViewport(0, 0, 280, 210); }
+
+void update_game() {
+  // do things
+}
+
+void update_renderer() {
+  glClearColor(0.f, 1.f, 0.f, 1.f);
+  glClear(GL_COLOR_BUFFER_BIT);
+}
